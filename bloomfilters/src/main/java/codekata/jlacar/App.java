d4f545abcf4881656180450a0ade006dc6eb95ab
Junilu Lacar <junilu.lacar@gmail.com>
2017-10-19 08:56:47 -0400

    Initialized Maven project

---
+++ bloomfilters/src/main/java/codekata/jlacar/App.java
@@ -0,0 +1,13 @@
+package codekata.jlacar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
